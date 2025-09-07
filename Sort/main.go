f85efda8fbdc55130762391e2647609b392a62d4
Morteza Aliyari <morteza@aliyari.me>
2025-09-07 11:49:09 -0700

    implement quick sort algorithm

--- Sort/main.go
+++ Sort/main.go
@@ -11,39 +11,40 @@ import (
 const SIZE = 10_000
 
 func main() {
-	random := make([]int, SIZE)
-	bestCase := make([]int, SIZE)
-	worstCase := make([]int, SIZE)
+	array := make([]int, SIZE)
 	for i := range SIZE {
-		random[i] = rand.Intn(SIZE)
+		array[i] = rand.Intn(SIZE)
 	}
-	copy(bestCase, random)
-	slices.Sort(bestCase)
-	copy(worstCase, bestCase)
-	slices.Reverse(worstCase)
 
-	fmt.Printf("%-15s %-10s %-15s %-15s %-15s\n", "Algorithm", "Case", "Elasped(ms)", "Swaps", "Iterations")
+	fmt.Printf("%-15s %-15s %-15s %-15s\n", "Algorithm", "Elasped(ms)", "Swaps", "Iterations")
 	fmt.Println(strings.Repeat("-", 70))
 
 	// Bubble Sort
-	measureExecution(bubbleSort, slices.Clone(random), "Bubble", "Random")
-	measureExecution(bubbleSort, slices.Clone(bestCase), "Bubble", "Best")
-	measureExecution(bubbleSort, slices.Clone(worstCase), "Bubble", "Worst")
+	measureExecution(bubbleSort, slices.Clone(array), "Bubble")
 
 	// Selection Sort
-	measureExecution(selectionSort, slices.Clone(random), "Selection", "Random")
-	measureExecution(selectionSort, slices.Clone(bestCase), "Selection", "Best")
-	measureExecution(selectionSort, slices.Clone(worstCase), "Selection", "Worst")
+	measureExecution(selectionSort, slices.Clone(array), "Selection")
 
 	// Insertion Sort
-	measureExecution(insertionSort, slices.Clone(random), "Insertion", "Random")
-	measureExecution(insertionSort, slices.Clone(bestCase), "Insertion", "Best")
-	measureExecution(insertionSort, slices.Clone(worstCase), "Insertion", "Worst")
+	measureExecution(insertionSort, slices.Clone(array), "Insertion")
+
+	// Quick Sort
+	measureExecution(quickSort, slices.Clone(array), "Quick")
 }
 
-func measureExecution(fn func([]int) (int, int), array []int, name, ratio string) {
+func measureExecution(fn any, array []int, name string) {
+	var swaps, iterations int
 	start := time.Now()
-	swaps, iterations := fn(array)
+	switch f := fn.(type) {
+	case func([]int) (int, int):
+		swaps, iterations = f(array)
+	case func([]int, int, int) (int, int):
+		swaps, iterations = f(array, 0, SIZE-1)
+	default:
+		panic("unsupported function!")
+
+	}
+
 	elasped := time.Since(start).Milliseconds()
-	fmt.Printf("%-15s %-10s %-15d %-15d %-15d\n", name, ratio, elasped, swaps, iterations)
+	fmt.Printf("%-15s %-15d %-15d %-15d\n", name, elasped, swaps, iterations)
 }
--- Sort/quick-sort.go
+++ Sort/quick-sort.go
@@ -1,5 +1,25 @@
 package main
 
-func quickSort(arr []int) (swaps, iterations int) {
+func quickSort(arr []int, low, high int) (swaps, iterations int) {
+	if low >= high {
+		return
+	}
+
+	pivotValue := arr[high]
+	i := low - 1
+	for j := low; j < high; j++ {
+		iterations++
+		if arr[j] <= pivotValue {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+			swaps++
+		}
+	}
+	i++
+	arr[i], arr[high] = arr[high], arr[i]
+	s1, i1 := quickSort(arr, low, i-1)
+	s2, i2 := quickSort(arr, i+1, high)
+	swaps += s1 + s2
+	iterations += i1 + i2
 	return
 }
